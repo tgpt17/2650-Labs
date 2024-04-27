f775c88786758041715e099f9785b7460929acc3
Travis Wagner <travgpt@gmail.com>
2024-04-27 17:16:47 -0500

    Week 11

--- Week 11/student-sign-in/src/main.js
+++ Week 11/student-sign-in/src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap'
+
+app.mount('#app')
