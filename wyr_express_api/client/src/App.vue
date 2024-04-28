94224b80d8117972ca47e309a1502d5d95655afe
Travis Wagner <travgpt@gmail.com>
2024-04-28 15:09:15 -0500

    Week 13

---
+++ wyr_express_api/client/src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+  // Importing the WouldYouRather component from its file
+  import WouldYouRather from './components/WouldYouRather.vue'
+
+  // Importing the ref function from Vue
+  import { ref } from 'vue'
+
+  // Creating reactive variables to store the question and answers
+  const wyrQuestion = ref('Live in a house shaped like a triangle or a house shaped like a circle?')
+  const wyrAnswer1 = ref('Triangle House')
+  const wyrAnswer2 = ref('Circle House')
+
+  // This will store the user's answer once they've made a choice
+  const userSelection = ref('')
+
+  // Function to update the user's selection
+  function updateUserSelection(userChoice) {
+    userSelection.value = userChoice
+  }
+</script>
+
+<template>
+
+<div id="app-component">
+  <h1>Would You Rather?</h1>
+
+  <!-- Using the WouldYouRather component -->
+  <WouldYouRather 
+    v-bind:question="wyrQuestion" 
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-selected="updateUserSelection">
+  </WouldYouRather>
+
+  <!-- Displaying the user's selection -->
+  <p>{{ userSelection }}</p>
+</div>
+
+</template>
+
+
+<style scoped>
+
+#app-component {
+  font-size: larger;
+  background-color: aqua;
+  padding: 40px;
+}
+
+p {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+</style>
+
---
+++ wyr_express_api/client/src/components/WouldYouRather.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+  import { ref } from 'vue'
+
+  // Defining props for the component
+  defineProps({
+    question: String, // Prop for the question text
+    answer1: String, // Prop for the first answer text
+    answer2: String // Prop for the second answer text
+  })
+
+  // Defining emits for the component
+  const emit = defineEmits([
+    'answer-selected' // Emit event for when an answer is selected
+  ])
+
+  // Creating a reactive variable to store the user's choice
+  const choice = ref('')
+
+  // Function to handle when a choice is made
+  function choiceMade() {
+    // Emitting an event to tell the parent component that the user has made a choice
+    emit('answer-selected', choice.value)
+  }
+</script>
+
+<template>
+  <div id="wyr">
+
+    <h2>Make Your Choice!</h2>
+
+    <p>{{ question }}</p>
+
+    <!-- Radio buttons for the two answers -->
+    <div>
+      <input v-model="choice" v-bind:value="answer1" v-on:change="choiceMade" type="radio" id="answer-1">
+      <label for="answer-1">{{ answer1 }}</label>
+
+      <input v-model="choice" v-bind:value="answer2" v-on:change="choiceMade" type="radio" id="answer-2">
+      <label for="answer-2">{{ answer2 }}</label>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+
+#wyr {
+  background-color: hotpink;
+}
+
+p {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+</style>
+
